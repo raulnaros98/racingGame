e81016b8db5cf467dfd5502ec5b2984c7cf1e577
raulnaros98 <raulnaros98@gmail.com>
2018-12-13 21:30:11 +0200

    sample text

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,10 +4,30 @@ package org.fasttrackit;
  * Hello world!
  *
  */
+//this is a comment
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Car car =new Car();
+       car.name = "Dacia";
+       car.color = "white";
+       car.doorCount = 4;
+       car.mileage = 7.5;
+       car.running = true;
+
+
+       Engine engine = new Engine();
+       engine.manufacturer ="Renault" ;
+       engine.capacity = 1500;
+
+       car.engine = engine;
+       engine = null;
+
+       Car car2 = new Car();
+       car2.name = "BMW";
+
     }
+
 }
