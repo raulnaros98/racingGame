a35f21a05cd20ab81ca376940a3d0cfb79fd8f3a
raulnaros98 <raulnaros98@gmail.com>
2019-01-10 19:04:38 +0200

    sample text

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,18 +6,30 @@ public class App
     public static void main( String[] args )
     {
 
-        Engine engine = new Engine();
-        engine.manufacturer ="Renault" ;
-        engine.capacity = 1500;
+//        Engine engine = new Engine();
+//        engine.manufacturer ="Renault" ;
+//        engine.capacity = 1500;
+//
+//        Car car =new Car(engine);
+//        car.name = "Dacia";
+//        car.color = "white";
+//        car.doorCount = 4;
+//        car.mileage = 7.5;
+//        car.running = true;
+//
+//        double distance =car.accelarate(100,0.5);
+        //example of static varables for the whole application
+        vehicle vehicle1 = new vehicle();
+       // vehicle1.vehicleCount++;
 
-        Car car =new Car(engine);
-       car.name = "Dacia";
-       car.color = "white";
-       car.doorCount = 4;
-       car.mileage = 7.5;
-       car.running = true;
+        vehicle vehicle2= new vehicle();
+        //vehicle2.vehicleCount++;
 
-       double distance =car.accelarate(100,0.5);
+
+        System.out.println( "Value from vehicle1 : "+ vehicle1.vehicleCount);
+        System.out.println( "Value from vehicle2 : "+ vehicle2.vehicleCount);
+        System.out.println( "Value from vehicle class : "+ vehicle.vehicleCount);
+
 
 
 
--- src/main/java/org/fasttrackit/vehicle.java
+++ src/main/java/org/fasttrackit/vehicle.java
@@ -1,11 +1,16 @@
 package org.fasttrackit;
 
 public class vehicle {
+    public static int vehicleCount;
     String name;
     String color;
     double mileage;
 
-    public double accelarate(double speed, double durationInHours) {
+    public vehicle() {
+        vehicleCount++;
+    }
+
+    protected double accelarate(double speed, double durationInHours) {
         System.out.println(name + "has accelarated with" + speed + "km/h for" + durationInHours + "hours");
         double travelDistance = speed * durationInHours;
         System.out.println("Travelled distance " + travelDistance +"km/h");
